f02362d3be4743d35c8f17cb743d74ac533dbe55
Gyan Lakhwani <gyanlakhwani@gmail.com>
2023-05-11 17:23:40 +0530

    pell editor

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ document.querySelectorAll('.tab').forEach(item => {
         event.target.classList.add('active');
     })
 })
+
+document.getElementById("bold").addEventListener("click", function() {
+    var editor = document.getElementById("editor");
+    var selection = window.getSelection();
+    var range = selection.getRangeAt(0);
+
+    var newNode = document.createElement("b");
+    newNode.appendChild(range.extractContents());
+    range.insertNode(newNode);
+
+    selection.removeAllRanges();
+    selection.addRange(range);
+  });
+
+
+
+
+
+
+
+
